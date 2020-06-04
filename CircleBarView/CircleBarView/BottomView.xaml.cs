44c6c1d14466c1cf538f202820ec0a59afaf476b
Anatoliy <ankonovalov@outlook.com>
2020-06-04 06:30:15 +0300

    Add arrow images, bottom view with tap recognizers

---
+++ CircleBarView/CircleBarView/BottomView.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CircleBarView
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class BottomView : ContentView
+    {
+        public BottomView()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+            openLabel.Text = "OOO yeah!";
+        }
+
+        private void SkipGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+            skipLabel.Text = "Oh, shit";
+        }
+    }
+}
